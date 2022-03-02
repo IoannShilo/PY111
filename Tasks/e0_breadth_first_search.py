0810915fe4c86b9bafd97ef8d5e0e6c7e2cdae4c
ioannshilo <ivan-shilo.89@mail.ru>
2022-03-02 19:26:26 +0300

    e0 done

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,4 +1,5 @@
 from typing import Hashable, List
+from collections import deque
 import networkx as nx
 
 
@@ -10,5 +11,24 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    path_nodes = []  # сюда должны помещаться узлы из очереди / "сгоревшие узлы" результат
+    visited_nodes = {node: False for node in g.nodes} # словарь из непосещенных узлов
+    # конец справа, начало слева
+    wait_nodes = deque()  # очередь из узлов которые ожидают обхода
+    wait_nodes.append(start_node)  # подожгли первый узел
+
+    while wait_nodes:
+        current_node = wait_nodes.popleft()  # достаем горящий узел, чтобы поджечь соседей, которые еще не горят
+        path_nodes.append(current_node) # формируем путь обхода
+        visited_nodes[current_node] = True # делаем узел сгоревшим
+
+        neighbours = g[current_node]
+        for neighbour in neighbours:
+            if not visited_nodes[neighbour]:
+                wait_nodes.append(neighbour) # подожгли соседа
+                visited_nodes[neighbour] = True
+
+    return path_nodes
+
+
+
